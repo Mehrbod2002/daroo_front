2fd605a7f79794579b0ceee939e6bdf5bcf2be82
mhmd <mhmd@gmail.com>
2023-12-12 11:51:28 +0330

    mhmd

---
+++ demo/main-assets/js/rule.js
@@ -0,0 +1,45 @@
+function getvizitReport() {
+  var url = urldemo + `/api/admin/rules/`;
+  try {
+    const request = new XMLHttpRequest();
+    request.onloadend = function () {
+      if (request.status == 200 || request.status == 201) {
+        var response = JSON.parse(this.responseText);
+        console.log(response[0].content);
+
+        document.querySelector("#ruleBody").innerHTML = response[0].content;
+      } else if (request.status == 400 || request.status == 403) {
+        const res = JSON.parse(request.response);
+        console.log(res);
+        const keys = Object.keys(res);
+        let msg = "";
+        keys.forEach((key, index) => {
+          console.log(`${res[key]}`);
+          msg = msg + `${res[key]}<br>`;
+        });
+        if (msg) {
+          $(".messagewrapper").fadeIn();
+          messageBox.innerHTML = msg;
+        }
+      } else {
+        $(".messagewrapper").fadeIn();
+        messageBox.innerHTML =
+          "<span class='text-sm text-danger'>متاسفانه مشکلی در سایت پیش آمده است لطفا بعدا تلاش کنید </span>";
+      }
+      setTimeout(clearMessageBox, 1000);
+    };
+
+    request.onloadstart = function () {
+      $(".loader").fadeIn();
+    };
+    request.open("GET", url);
+    request.setRequestHeader(
+      "Authorization",
+      `Token ${localStorage.getItem("token")}`
+    );
+    request.send();
+  } catch (error) {
+    console.error(error);
+  }
+}
+getvizitReport();
