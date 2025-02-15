ff06ef2847d66dfecbc7727b6bed2c38bde53792
Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>
2025-02-15 12:34:55 +0000

    faeat
    Signed-off-by: Amirhossein Akhlaghpour <m9.akhlaghpoor@gmail.com>

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from http.server import SimpleHTTPRequestHandler, HTTPServer
-import os
+import os, time
 
 class CustomHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
+        time.sleep(5)
         if self.path == "/" or os.path.isdir(self.translate_path(self.path)):
             self.path = "/main-index.html"
         super().do_GET()
